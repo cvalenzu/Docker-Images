d89e268a7105892020f366d996e985f27934f626
Camilo <cacke91@gmail.com>
2017-10-02 01:05:57 -0300

    Chaged jdk installation and added build script

---
+++ acs_builder/centos6/build.sh
@@ -0,0 +1,13 @@
+cd /ACS
+source LGPL/acsBUILD/config/.acs/.bash_profile.acs
+
+#Compiling ExtProds
+cd ExtProd/PRODUCTS
+./download-products.sh
+cd ../INSTALL
+./buildTools
+
+#Compiling ACS
+cd /ACS
+export MAKE_NOSTATIC=yes; export MAKE_NOIFR_CHECK=on
+make
