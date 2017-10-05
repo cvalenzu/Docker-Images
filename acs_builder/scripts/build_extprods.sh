2204961eec18bd95143cb1080a674e9086e0beb9
Camilo Valenzuela <cacke91@gmail.com>
2017-10-05 13:48:23 -0300

    Added system ant

--- acs_builder/scripts/build_extprods.sh
+++ acs_builder/scripts/build_extprods.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 cd /ACS
 
-# Clean the directory where the tools will be built.
-git clean -d -f LGPL
 # Source the bash profile needed to set environment variables to download and compile ACS
 source LGPL/acsBUILD/config/.acs/.bash_profile.acs
 # Delete the previous dir where ACS was installed.
@@ -10,19 +8,16 @@ rm -rf $ALMASW_INSTDIR
 # Create the dir where ACS going to be installed.
 mkdir -p $ALMASW_INSTDIR
 
-
 #Compiling ExtProds
 cd ExtProd/PRODUCTS
 find . -name "config.cache" | xargs rm -rf
 
 ./download-products.sh
 cd ../INSTALL
-./buildTools
+sed -i '210s/.*/\techo "Skipping Ant"/' buildTools
+
+./buildTools  2>&1 |tee build.summary
+
+git chechout buildTools
 cd $ALMASW_INSTDIR
 find -name "*.o" | xargs rm -rf
-
-#Compiling ACS
-cd /ACS
-export MAKE_NOSTATIC=yes; export MAKE_NOIFR_CHECK=on
-# Paste the output logs from jenkins
-make 2>&1 |tee build.summary
